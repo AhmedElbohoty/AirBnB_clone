457f8bf27db4d49e11507e7c0cebfcc28b6f0615
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-10 16:27:47 +0200

    fix issue

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,8 @@ class BaseModel():
         '''
         if len(kwargs) == 0:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
             models.storage.new(self)
             return
@@ -59,7 +59,7 @@ class BaseModel():
 
     def save(self):
         '''Updates the public instance attribute updated_at'''
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
         models.storage.save()
 
