e4867329a4eca4a944aabf7e88703098a2602d9e
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-12 01:17:24 +0200

    Add more tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -4,6 +4,81 @@ import unittest
 import datetime
 import models
 from models.review import Review
+from models.place import Place
+
+
+class TestReviewBaseModel(unittest.TestCase):
+    '''Unit tests for methods and attributes inherited from base model'''
+
+    @classmethod
+    def setUpClass(cls):
+        '''Update file path for test'''
+        models.storage.update_file_path('test_file.json')
+
+    @classmethod
+    def tearDownClass(cls):
+        '''Update file path for app'''
+        models.storage.update_file_path('file.json')
+
+    def tearDown(self):
+        models.storage.reset()
+
+    def test_review_id(self):
+        '''Check if id creation wroks well'''
+        review_1 = Review()
+        review_2 = Review()
+        self.assertIsInstance(review_1, Review)
+        self.assertTrue(hasattr(review_1, 'id'))
+        self.assertIsInstance(review_1.id, str)
+        self.assertNotEqual(review_1.id, review_2.id)
+
+    def test_date(self):
+        '''Tests for created_at and updated_at attributes'''
+        review = Review()
+        self.assertTrue(hasattr(review, 'created_at'))
+        self.assertTrue(hasattr(review, 'updated_at'))
+        self.assertIsInstance(review.created_at, datetime.datetime)
+        self.assertIsInstance(review.updated_at, datetime.datetime)
+
+    def test_save(self):
+        '''Tests for public 'save' method'''
+        review = Review()
+        old_updated_at = review.updated_at
+        key = 'Review.{}'.format(review.id)
+        self.assertNotIn(key, models.storage.all().values())
+        review.save()
+        self.assertIn(key, models.storage.all().keys())
+        self.assertTrue(old_updated_at < review.updated_at)
+
+    def test_re_create_instance(self):
+        '''Tests for re-creating an instance'''
+        review = Review()
+        review.number = 98
+        review_json = review.to_dict()
+        new_review = Review(**review_json)
+
+        self.assertEqual(new_review.id, review.id)
+        self.assertEqual(new_review.created_at, review.created_at)
+        self.assertEqual(new_review.updated_at, review.updated_at)
+        self.assertEqual(new_review.number, review.number)
+
+        new_review.number = 100
+        new_review.save()
+        self.assertNotEqual(new_review.number, review.number)
+        self.assertLess(review.updated_at, new_review.updated_at)
+
+        self.assertFalse(new_review is review)
+
+    def test_to_dict(self):
+        '''Tests for 'to_dict' method'''
+        review = Review()
+        review_json = review.to_dict()
+        self.assertEqual(review_json['__class__'], 'Review')
+        self.assertEqual(review_json['id'], review.id)
+        self.assertEqual(review_json['created_at'],
+                         review.created_at.isoformat())
+        self.assertEqual(review_json['updated_at'],
+                         review.updated_at.isoformat())
 
 
 class TestReview(unittest.TestCase):
@@ -30,6 +105,18 @@ class TestReview(unittest.TestCase):
         self.assertTrue(hasattr(review_1, 'id'))
         self.assertIsInstance(review_1.id, str)
         self.assertNotEqual(review_1.id, review_2.id)
+
+    def test_review_place_id(self):
+        '''Check if id creation wroks well'''
+        review = Review()
+        place = Place()
+
+        review.place_id = place.id
+
+        self.assertIsInstance(review, Review)
+        self.assertEqual(review.place_id, place.id)
+        self.assertTrue(hasattr(review, 'place_id'))
+        self.assertIsInstance(review.place_id, str)
 
     def test_date(self):
         '''Tests for created_at and updated_at attributes'''
